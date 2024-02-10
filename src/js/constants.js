0913736a7d5a86421cdd41aa3db86353c7857dc6
Juan Pablo Gama Olivares <juanpablo.gama.olivares@gmail.com>
2024-02-10 14:55:22 -0700

    add: constants js

---
+++ src/js/constants.js
@@ -0,0 +1,29 @@
+export const tileSources = [
+  {
+    id: 1,
+    name: 'rock',
+    mediaSrc: 'images/icon-rock.svg'
+  },
+  {
+    id: 2,
+    name: 'paper',
+    mediaSrc: 'images/icon-paper.svg'
+  },
+  {
+    id: 3,
+    name: 'scissors',
+    mediaSrc: 'images/icon-scissors.svg'
+  },
+  {
+    id: 4,
+    name: 'lizard',
+    mediaSrc: 'images/icon-lizard.svg'
+  },
+  {
+    id: 5,
+    name: 'spock',
+    mediaSrc: 'images/icon-spock.svg'
+  }
+]
+
+export const modal = document.querySelector('.modal')
