1fdd09db3a5e988d44d0f60853e8bc57c81d0731
Juan Pablo Gama Olivares <juanpablo.gama.olivares@gmail.com>
2024-02-10 14:50:21 -0700

    feat: step 1 structure for game field

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
-import { useState } from 'react'
+/* eslint-disable no-unused-vars */
 import './css/App.css'
+import { useState } from 'react'
+
 import { Score } from './components/Score.jsx'
-import { increaseScore, decreaseScore } from './js/utils.js'
+import { Tile } from './components/Tile.jsx'
+import { Modal } from './components/Modal.jsx'
+
+import { increaseScore, decreaseScore, manageModal } from './js/utils.js'
+import { tileSources, modal } from './js/constants.js'
 
 export function App () {
   const [actualScore, setActualScore] = useState(0)
@@ -13,13 +19,18 @@ export function App () {
         <Score actualScore={actualScore} />
       </header>
       <main className='main'>
-        <button onClick={() => increaseScore({ actualScore, setActualScore })}>
-          Increase
-        </button>
-        <button onClick={() => decreaseScore({ actualScore, setActualScore })}>
-          Decrease
-        </button>
+        <section className='game-field'>
+          {
+            tileSources.map(tile => {
+              return <Tile key={tile.id} src={tile.mediaSrc} name={tile.name} />
+            })
+          }
+        </section>
+        <Modal name='rules' imgSrc='images/image-rules-bonus.svg' closeIcon='images/icon-close.svg' modal={modal} />
       </main>
+      <footer className='footer'>
+        <button className='btn' onClick={() => { manageModal(modal) }}>rules</button>
+      </footer>
     </>
   )
 }
